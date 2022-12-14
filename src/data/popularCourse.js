36995abc5258c04fe48ae99b1a50f96a9f295aaa
Muhammad Akib Jabed <muhammadakibjabed@gmail.com>
2022-12-14 19:43:43 +0600

    first commit

---
+++ src/data/popularCourse.js
@@ -0,0 +1,31 @@
+const popularCourse=[
+    {
+        name:'Complete Web Development Course With Jhankar Mahbub',
+        id:1,
+        img:'https://thefinancialexpress.com.bd/uploads/1669636549.jpg',
+        instructor:'Jhankar Mahbub',
+        price:'200',
+    },
+    {
+        name:'Learn MERN like a pro',
+        id:2,
+        img:'https://res.cloudinary.com/practicaldev/image/fetch/s--0FRJGdyZ--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/epv55hgtsfi8csprpj9u.jpg',
+        instructor:'Fayzul Karim',
+        price:'120',
+    },
+    {
+        name:'Professional grapichs design course',
+        id:3,
+        img:'https://img.freepik.com/premium-vector/banner-design-professional-graphic-designing-cartoon-style-template_262129-6402.jpg?w=2000',
+        instructor:'Mehrab uddin',
+        price:'100',
+    },
+    {
+        name:'Learn UI/UX',
+        id:4,
+        img:'https://wallpaperaccess.com/full/6000163.jpg',
+        price:'80',
+    },
+
+
+]
