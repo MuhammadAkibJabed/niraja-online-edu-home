4014f91e4729c913be32b3c69e322f77a8f16d84
Muhammad Akib Jabed <muhammadakibjabed@gmail.com>
2022-12-19 12:41:59 +0600

    changes added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './components/Home/Home';
 import Courses from './components/Courses/Courses';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <Route path='/courses' element={<Courses></Courses>}></Route>
       <Route path='/about' element={<About></About>}></Route>
       <Route path='/contact' element={<Contact></Contact>}> </Route>
+      <Route path='*' element={<NotFound></NotFound>}></Route>
      </Routes>
      <Footer></Footer>
     </div>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1 className='text-9xl text-bluish'>404 Page not found</h1>
+            
+        </div>
+    );
+};
+
+export default NotFound;
